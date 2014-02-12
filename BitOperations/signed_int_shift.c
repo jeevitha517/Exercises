91bc3bbfa13c852805933496e7eddde0abc81f2d
Kiran Poola <kiran.poola@bigswitch.com>
2014-02-11 23:46:09 -0800

    Program to print signed integer value in hex.

---
+++ BitOperations/signed_int_shift.c
@@ -0,0 +1,13 @@
+/* Program to understand how signed integer will be stored in memory. */
+
+#include <stdio.h>
+
+int main()
+{
+    int val = -1;
+
+    printf("Value : 0x%x\n", val);
+
+    return 0;
+}
+
