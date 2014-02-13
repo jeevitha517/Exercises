7057649ed5a8f2df2684a60a9be1673b50ba8786
jeevitha <jeevitha.r.agaram@gmail.com>
2014-02-12 23:25:54 -0800

    Checked with different combinations and fixed the bug

--- String_functions/case_insensitive_string_compare.c
+++ String_functions/case_insensitive_string_compare.c
@@ -2,9 +2,14 @@
 
 int check_character_by_character(char ch1, char ch2)
 {
-    if((ch1 - ch2 == 32) || (ch1 - ch2 == -32)) {
-        return 0;
+    if ( (((ch1 >= 'A') && (ch1 <= 'Z')) || ((ch1 >= 'a') && (ch1 <= 'z'))) &&
+         (((ch2 >= 'A') && (ch2 <= 'Z')) || ((ch2 >= 'a') && (ch2 <= 'z'))) ) {
+
+        if ((ch1 - ch2 == 32) || (ch1 - ch2 == -32)) {
+            return 0;
+        }
     }
+
     return (ch1 - ch2);
 }    
     
@@ -35,8 +40,14 @@ int i_strcmp(char *s1, char *s2)
 
 int main()
 {
-    char s1[20] = "HELLO";
-    char s2[20] = "hello123";
-    printf("%d\n", i_strcmp(s1, s2));
+    char s1[20] = "AA@B";
+    char s2[20] = "AA b";
+    printf("%d\n", i_strcmp("AA@B", "AA b"));
+    printf("%d\n", i_strcmp("HELLO", "hello"));
+    printf("%d\n", i_strcmp("HeLLo", "hEllO"));
+    printf("%d\n", i_strcmp("HELLO123", "hello1"));
+    printf("%d\n", i_strcmp("HELLO", "hello123"));
+    printf("%d\n", i_strcmp("", "hello"));
+    printf("%d\n", i_strcmp("HELLO", ""));
     return 0;
 }
