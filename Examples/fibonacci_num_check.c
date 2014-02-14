2d984a82145c89a81484c59c1b9ec7bf95e56d58
jeevitha <jeevitha.r.agaram@gmail.com>
2014-02-13 16:01:14 -0800

    Changed program into function

--- Examples/fibonacci_num_check.c
+++ Examples/fibonacci_num_check.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
-int main()
+int fibonacci_num_check(int num)
 {
-    int n1 = 0, n2 = 1, temp, num, flag = 0, sum = 0;
-    
-    printf("Enter a number to find whether num is Fibonacci num or not : ");
-    scanf("%d", &num);
-
+    int n1 = 0, n2 = 1, temp, flag = 0, sum = 0;
     while (sum <= num) {
         if(sum == num) {
             flag = 1;
@@ -18,11 +14,26 @@ int main()
     }
     
     if (flag == 1) {
+        return 1;
+    }
+
+    return 0;
+}    
+
+int main()
+{
+    int num, rv;  
+    printf("Enter a number to find whether num is Fibonacci num or not : ");
+    scanf("%d", &num);
+
+    rv = fibonacci_num_check(num);
+
+    if(rv == 1) {
         printf("%d is fibonacci number\n", num);
     }
     else {
         printf("%d is not fibonacci number\n", num);
     }
-
+    
     return 0;
-}    
+}
